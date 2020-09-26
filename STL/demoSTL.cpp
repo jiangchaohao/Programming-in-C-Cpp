f1a1745fd98e6617cfaa83268ea9175e7d0a67ac
SudhakarKuma <sm221294@gmail.com>
2020-09-27 01:03:15 +0530

    Add sets

--- STL/demoSTL.cpp
+++ STL/demoSTL.cpp
@@ -12,7 +12,10 @@ bool f(int x, int y) {
   return x > y;
 }
 
-int main(){
+/* vectors using STL */
+void vectorDemo(){
+
+  cout << "vectors using STL" << endl;
 
   // Declare a vector using STL
   vector<int> A = {11, 2, 3, 14};
@@ -51,10 +54,12 @@ int main(){
   // Find the first occurence of element 100 in vector A
   // Get the iterator to the first occurence of element 100 in vector A
   // Apply lower_bound which returns an iterator to the first element >= 100
-  vector<int>::iterator it = lower_bound(A.begin(), A.end(), 100);
+  // vector<int>::iterator it = lower_bound(A.begin(), A.end(), 100);
+  auto it = lower_bound(A.begin(), A.end(), 100);
 
   // Apply upper_bound which returns an iterator to the first element > 100
-  vector<int>::iterator it2 = upper_bound(A.begin(), A.end(), 100);
+  // vector<int>::iterator it2 = upper_bound(A.begin(), A.end(), 100);
+  auto it2 = upper_bound(A.begin(), A.end(), 100);
 
   // Print the contents of it and it2
   cout << *it << " " << *it2 << endl;
@@ -83,4 +88,110 @@ int main(){
     cout << x << " ";
   }
   cout << endl;
+
+  // Reiterate and simultaneously update the values of vector A
+  for (int &x : A){
+    x++;
+    // cout << x << " ";
+  }
+  // cout << endl;
+
+  // Check whether the values in vector A have been updated
+  for (int x : A){
+    cout << x << " ";
+  }
+  cout << endl;
+
+  /* In case of vectors, we need to sort the elements first before applying the
+  lower_bound and upper_bound operations. What if we want to insert the elements
+  in a random order? Like insert 2, 4, 1, etc and after each operation, we want
+  to have queries in between. For this, we can go for a set.
+  */
 }
+
+/* sets using STL */
+void setDemo(){
+
+  cout << "\n sets using STL" << endl;
+
+  // Declare a set using STL
+  set<int> S;
+
+  // Insert a number in set S
+  S.insert(1);
+  S.insert(2);
+  S.insert(-1);
+  S.insert(-10);
+  // Set maintains the ascending order sequence
+
+  // Print the elements of set S
+  for (int x : S) {
+    cout << x << " ";
+  }
+  cout << endl;
+  // S = {-10, -1, 1 ,2}
+
+  // Check whether -1 is present in the set S
+  auto it = S.find(-1); // The iterator it will point to the location of -1
+
+  /*If some element is not present, the find function will return S.end() */
+  if (it == S.end()) cout << "Not present! \n";
+  else {
+    cout << "Present\n";
+    cout << *it << endl;
+  }
+
+  // Find the first number in the set S which is greater than or equal to -1
+  auto it2 = S.lower_bound(-1); // It will return an iterator
+
+  // Find the first number in the set S which is strictly greater than -1
+  auto it3 = S.upper_bound(-1); // It will return an iterator
+
+  // Print the contents of the iterators
+  cout << *it2 << " " << *it3 << endl;
+
+  // What if we try to find the upper_bound of 2
+  // The upper_bound of 2 does not exist in the set S
+  auto it4 = S.upper_bound(2);
+  if(it4 == S.end()) cout << "Couldn't find something like that!" << endl;
+}
+
+
+/* maps using STL */
+void mapDemo(){
+  cout << "\n maps using STL" << endl;
+
+  // Declare a map using STL
+  map<int, int> M;
+
+}
+
+/* Driver function */
+int main(){
+  vectorDemo();
+  setDemo();
+  mapDemo();
+}
+
+
+/*
+vectors using STL
+2
+2 3 11 14
+1
+0
+100 123
+5
+123 100 100 100 100 100 14 11 3 2
+123 100 100 100 100 100 14 11 3 2
+124 101 101 101 101 101 15 12 4 3
+
+ sets using STL
+-10 -1 1 2
+Present
+-1
+-1 1
+Couldn't find something like that!
+
+ maps using STL
+*/
